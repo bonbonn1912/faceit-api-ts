be9a34cb567703f6892ff9fec73566e29efb86fd
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2022-01-28 21:52:28 +0100

    correct sort

--- src/server/firestore/getLastPlayers.ts
+++ src/server/firestore/getLastPlayers.ts
@@ -6,7 +6,7 @@ const db = Client.firestore;
 
 export default async function getPlayerEntries() {
     return new Promise<any>((resolve, reject) => {
-        db.collection('players').limit(8).get().then((snapshot: { docs: any[]; }) => {
+        db.collection('players').orderBy('timestamp', 'desc').limit(8).get().then((snapshot: { docs: any[]; }) => {
            var entries : any = snapshot.docs.map(doc => doc.data());
          //  var reversedEntries = entries.reverse();
            resolve(entries);
