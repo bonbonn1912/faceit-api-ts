4e0151bb2c1aee201a61d6d035e31a2626b19395
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2022-01-29 22:25:53 +0100

    Update closeMatchroom.ts

--- src/server/firestore/closeMatchroom.ts
+++ src/server/firestore/closeMatchroom.ts
@@ -5,9 +5,14 @@ const db = Client.firestore;
 
 export default async function closeMatchRoomInFirestore(documentid: string)  {
     console.log(documentid);
-    db.collection('matches').doc(documentid).update({
+    try{
+      db.collection('matches').doc(documentid).update({
         [`match.isRunning`]: false
       });
+    }catch(e){
+      console.log("Dokument konnte nicht gelöscht werden");
+    }
+   
 };
 
 
